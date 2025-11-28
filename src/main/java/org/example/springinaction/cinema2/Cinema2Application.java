06a44894757d5ac9a4daf64d6215539ee5a14b4a
tren <radmir15342@gmail.com>
2025-11-28 19:30:05 +0300

    first commit

---
+++ src/main/java/org/example/springinaction/cinema2/Cinema2Application.java
@@ -0,0 +1,13 @@
+package org.example.springinaction.cinema2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cinema2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Cinema2Application.class, args);
+	}
+
+}
